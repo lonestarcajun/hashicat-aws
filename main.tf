48f106a5033056a79e2f11eb0a957db6bad2e14e
Bart Pouson <pousb@yahoo.com>
2020-06-10 17:44:36 +0000

    Updated my code

--- main.tf
+++ main.tf
@@ -189,4 +189,9 @@ locals {
 resource aws_key_pair "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
+
+  tags = {
+    Billable   = "true"
+    Department = "devops"
+  }
 }
